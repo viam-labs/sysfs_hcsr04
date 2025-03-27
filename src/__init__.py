78a71ddd74b1f8865e36a0f686eb7b81461c81b8
Matt Vella <mcvella@gmail.com>
2025-03-26 20:59:51 -0400

    SUBTYPE->API

--- src/__init__.py
+++ src/__init__.py
@@ -7,4 +7,4 @@ from viam.resource.registry import Registry, ResourceCreatorRegistration
 
 from .sysfs_hcsr04 import HCSR04
 
-Registry.register_resource_creator(Sensor.SUBTYPE, HCSR04.MODEL, ResourceCreatorRegistration(HCSR04.new, HCSR04.validate))
+Registry.register_resource_creator(Sensor.API, HCSR04.MODEL, ResourceCreatorRegistration(HCSR04.new, HCSR04.validate))
--- src/__main__.py
+++ src/__main__.py
@@ -10,7 +10,7 @@ async def main():
     Resources must be pre-registered. For an example, see the `__init__.py` file.
     """
     module = Module.from_args()
-    module.add_model_from_registry(Sensor.SUBTYPE, HCSR04.MODEL)
+    module.add_model_from_registry(Sensor.API, HCSR04.MODEL)
     await module.start()
 
 if __name__ == "__main__":
